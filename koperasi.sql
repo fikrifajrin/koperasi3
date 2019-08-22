e6292f5756bd0376a48c48860a7a52616866aee3
fikrifajrin <fikrifajrin27@gmail.com>
2019-08-22 21:42:14 +0700

    Add files via upload

---
+++ koperasi.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.9-MariaDB : Database - koperasi
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`koperasi` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `koperasi`;
+
+/*Table structure for table `anggota` */
+
+DROP TABLE IF EXISTS `anggota`;
+
+CREATE TABLE `anggota` (
+  `no_ktp` int(11) DEFAULT NULL,
+  `nama_anggota` varchar(100) DEFAULT NULL,
+  `jenis_kel` varchar(50) DEFAULT NULL,
+  `tgl_lahir` date DEFAULT NULL,
+  `foto` varchar(500) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  UNIQUE KEY `UNIQUE` (`no_ktp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `anggota` */
+
+insert  into `anggota`(`no_ktp`,`nama_anggota`,`jenis_kel`,`tgl_lahir`,`foto`,`alamat`) values (991122,'Maimun','Laki-laki','2019-08-05','2208201913474400_pernillesnedkerhansen.jpg','kjbhedjgh jkrjgwrklg klwergk;l'),(1234567890,'Sarkijan','Perempuan','1994-07-05','220820191434061200px-Adidas_Logo.svg.png','Jl. Kambing no 24 Gresik');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
